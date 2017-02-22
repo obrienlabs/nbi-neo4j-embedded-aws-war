5d8cd9bca08a47e9547dd983750d7d3aa8cf131b
Michael O'Brien <michael@obrienlabs.org>
2017-02-21 22:44:38 -0500

    #6 add batch, relationship, thread start/end performance factors to fork join bolt code

--- src/main/java/org/obrienlabs/nbi/presentation/ForkJoinGraphServer.java
+++ src/main/java/org/obrienlabs/nbi/presentation/ForkJoinGraphServer.java
@@ -18,6 +18,10 @@ public class ForkJoinGraphServer {
     private static String pass;
     private static int power;
     private static int threadMult;
+    private static long batchSize;
+    private static boolean relationships;
+    private static int threadStart;
+    private static int threadEnd;
     // serial (batch 100)
     // 16384 1t 265 sec 12%
     
@@ -26,20 +30,20 @@ public class ForkJoinGraphServer {
     // 14 16 th 89 sec 80%
     // 14 32 th 27 sec 60%
     
-	public void compute(long startId, long endId, long length) {
-		ForkJoinGraphUnitOfWork forkJoinUOW = new ForkJoinGraphUnitOfWork(startId, endId, length,pass, port, ip, threadMult);
-		int threads = Runtime.getRuntime().availableProcessors() * threadMult;
-		long startTime = System.currentTimeMillis();
+	public void compute(long startId, long endId, long length, int threads) {
+		ForkJoinGraphUnitOfWork forkJoinUOW = new ForkJoinGraphUnitOfWork(startId, endId, length,pass, port, ip, threadMult, batchSize, relationships);
+//		int threads = Runtime.getRuntime().availableProcessors() * threadMult;
 		// create a ppol of threads to the power of pool * # of (proc + ht)
-		mapReducePool = new ForkJoinPool(1);//threads);
+		mapReducePool = new ForkJoinPool(threads);
+		long startTime = System.currentTimeMillis();
 		mapReducePool.invoke(forkJoinUOW);
 		long endTime = System.currentTimeMillis();
 		System.out.println(new StringBuffer(
 				String.valueOf(endTime - startTime)).append(",")
 				.append(threads).append(",")
-				.append(threads).append(",")
 				.append(String.valueOf((endTime - startTime) / 1000))
-				.append(threads).append(",").append(power).toString());
+				.append(",").append(power)
+				.append(",").append(batchSize).toString());
 	}
 	
 	 public void clean(boolean delete, int power) {
@@ -61,39 +65,55 @@ public class ForkJoinGraphServer {
 	 } 
 	 
 	public static void main(String[] args) {
-	    System.out.println("ForkJoinGraphServer threadMult, dim, del, pass, port, ip (v 20170208)");
+	    System.out.println("ForkJoinGraphServer threadMult, dim, del, pass, port, ip threadMult, batchSizeMax, relationships (v 20170208)");
 		int del = 0;
+		int relate = 0;
 		boolean delete = false;
-		if (args.length > 5) {
-		    power = Integer.parseInt(args[0]);
-	        del = Integer.parseInt(args[1]);
-	        pass = args[2];
-	        port = args[3];
-	        ip = args[4];
-	        threadMult = Integer.parseInt(args[5]);
+		long batchSizeMax = 11;
+		if (args.length > 9) {
+			threadStart =  Integer.parseInt(args[0]);
+			threadEnd=  Integer.parseInt(args[1]);
+		    power = Integer.parseInt(args[2]);
+	        del = Integer.parseInt(args[3]);
+	        pass = args[4];
+	        port = args[5];
+	        ip = args[6];
+	        threadMult = Integer.parseInt(args[7]);
+	        batchSizeMax = Long.parseLong(args[8]);
+	        relate =  Integer.parseInt(args[9]);
+	        
+	        if(relate > 0) {
+	        	relationships = true;
+	        }
 	        if(del > 0) {
 	        	delete = true;
 	        }
 		} else {
-			System.out.println("Usage dimensions pre-delete/0/1 password port");
+			System.out.println("Usage: thread-start, thread-end threadMult, dimensions pre-delete/0/1 password port ip threadMult batchSizeMax  relationships");
 			System.exit(1);;
 		}
 		System.out.println("availableProc\t: " + Runtime.getRuntime().availableProcessors());
-		System.out.println("fjps threads\t: " + power + " thread mult " + threadMult);
+		System.out.println("fjps power\t: " + power + " thread mult " + threadMult);
+		System.out.println("fjps threadStart\t: " + threadStart + " threadEnd " +  threadEnd);
 		System.out.println("freeMemory()\t: " + Runtime.getRuntime().freeMemory());
 		System.out.println("maxMemory()\t: " + Runtime.getRuntime().maxMemory());
 		System.out.println("totalMemory()\t: " + Runtime.getRuntime().totalMemory());
 		System.out.println("System.getEnv()\t: " + System.getenv().toString());
 		ForkJoinGraphServer server = new ForkJoinGraphServer();
 		//long extent = 25;
-		server.clean(delete, power);
 		//System.out.println("Range: bits\t: " + extent);
 		//for(long r=0;r<runs;r++) {
-			//for(long p=poolStart;p<poolEnd + 1;p++) {
-				//for(long i=3;i<extent + 1;i++) {
-					server.compute(0L, Long.valueOf(1 << power), Long.valueOf(1 << power));	
-				//}
-			//}
+			for(int threadP = threadStart; threadP < threadEnd; threadP++) {
+				for(long batchSizePower=0; batchSizePower < batchSizeMax + 1; batchSizePower++) {
+					batchSize = 1 << batchSizePower;
+					server.clean(delete, power);
+					//int threads = Runtime.getRuntime().availableProcessors() * threadMult;
+					server.compute(0L, Long.valueOf(1 << power), Long.valueOf(1 << power), 1 << threadP);	
+					try {
+						Thread.sleep(15000);
+					} catch( Exception e) {}
+				}
+			}
 		//}
 	}
 }
--- src/main/java/org/obrienlabs/nbi/presentation/ForkJoinGraphUnitOfWork.java
+++ src/main/java/org/obrienlabs/nbi/presentation/ForkJoinGraphUnitOfWork.java
@@ -20,10 +20,11 @@ public class ForkJoinGraphUnitOfWork extends RecursiveAction {
     private static final String username =  "neo4j";
     private String port;
     private String pass; 
-    private static long batchSize = 512;
+    private long batchSize;
     private int threadMult;
+    private boolean rel;
 	
-	public ForkJoinGraphUnitOfWork(long start, long _end, long _length, String pass, String port, String ip, int threadMult) {
+	public ForkJoinGraphUnitOfWork(long start, long _end, long _length, String pass, String port, String ip, int threadMult, long batchSize, boolean rel) {
 		startId = start;
 		length = _length;	
 		endId = _end;
@@ -31,6 +32,8 @@ public class ForkJoinGraphUnitOfWork extends RecursiveAction {
 		this.port = port;
 		this.threadMult = threadMult;
 		this.pass = pass;
+		this.batchSize = batchSize;
+		this.rel = rel;
 	}	
 	
 	private void endTransaction(Transaction transaction) {
@@ -53,29 +56,31 @@ public class ForkJoinGraphUnitOfWork extends RecursiveAction {
 	}
 	
 	protected void computeNoFork() {
-		System.out.println(new StringBuffer(String.valueOf(System.currentTimeMillis())).append(", start: ").append(startId)
-				.append(" end: ").append(endId).append(" length: ").append(length).toString());
+		//System.out.println(new StringBuffer(String.valueOf(System.currentTimeMillis())).append(", start: ").append(startId)
+		//		.append(" end: ").append(endId).append(" length: ").append(length).toString());
 		Driver driver = GraphDatabase.driver("bolt://" + ip + ":" + port,  AuthTokens.basic(username, pass)); 
 		try (Session session = driver.session()) {
 			create(startId,  0, session);
-			long batchCount = 0;
-			long batchMax = 100;	
+			//long batchCount = 0;
+			//ong batchMax = 100;	
 			Transaction transaction = session.beginTransaction();
 			for(long link=startId + 1; link<endId; link++) {
         		transaction.run( "CREATE (a:Node0 {name: {pk}})", parameters( "pk", link));
-        		linkUnidirectionalNoTx(transaction, "L0", 0, 0, link - 1, link);
-        		if(batchCount < batchMax) {
-        			batchCount++;
-        		} else {
-        			batchCount = 0;
-        			endTransaction(transaction);
-        			transaction = session.beginTransaction();
+        		if(rel) {
+        			linkUnidirectionalNoTx(transaction, "L0", 0, 0, link - 1, link);
         		}
+        		//if(batchCount < batchMax) {
+        		//	batchCount++;
+        		//} else {
+        		//	batchCount = 0;
+        		//	endTransaction(transaction);
+        		//	transaction = session.beginTransaction();
+        		//}
 			}
 			endTransaction(transaction);
 		}
 		driver.close();
-		System.out.println(new StringBuffer(String.valueOf(System.currentTimeMillis())).append(", end ").append(endId).toString());
+		//System.out.println(new StringBuffer(String.valueOf(System.currentTimeMillis())).append(", end ").append(endId).toString());
 	}
 	
     @Override
@@ -89,8 +94,8 @@ public class ForkJoinGraphUnitOfWork extends RecursiveAction {
 	    long split = length >> 1;
 	    // recursive case
 	    invokeAll(
-	    		new ForkJoinGraphUnitOfWork(startId, startId + split, split, pass, port, ip, threadMult),
-	    		new ForkJoinGraphUnitOfWork(startId + split, endId, split, pass, port, ip, threadMult)
+	    		new ForkJoinGraphUnitOfWork(startId, startId + split, split, pass, port, ip, threadMult, batchSize, rel),
+	    		new ForkJoinGraphUnitOfWork(startId + split, endId, split, pass, port, ip, threadMult, batchSize, rel)
 	    );		
 	}
 }
